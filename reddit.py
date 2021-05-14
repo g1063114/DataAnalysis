eb4edb82d6edcc7ab552fe4faf32b267fc3591e5
jinho <g1063114@naver.com>
2021-05-14 15:17:27 +0900

    Reddit scrapper!!

--- reddit.py
+++ reddit.py
@@ -26,17 +26,31 @@ def main():
 @app.route("/read", methods=['POST','GET'])
 def read():
     items=[]
+    param=[]
     if request.method == 'GET':
         for i in subreddits:
             if request.args.get(i) != None:
+                param.append(i)
                 reddit_url = requests.get(url+f"/r/{i}/top/?t=month",headers=headers)
                 content = BeautifulSoup(reddit_url.text,"html.parser")
-                items = content.find_all("a",{"data-click-id":"body"})
-
-                print(items)
-             
-    return render_template("read.html")
-
+                div = content.find("div","rpBJOHq2PR60pnwJlUyP0")
+                for item in div:
+                    if item.find("a",{"data-click-id":"body"}):
+                        vote = item.find("div","_1rZYMD_4xY3gRcSS3p8ODO _3a2ZHWaih05DgAOtvu6cIo").text
+                        if "k" in vote:
+                            vote = float(vote.replace("k","")) * 1000
+                        else:
+                            items.append({
+                                'url'   : item.find("a",{"data-click-id":"body"}).attrs['href'],
+                                'title' : item.find("a",{"data-click-id":"body"}).text,
+                                'vote'  : float(vote),
+                                'lang'  : i
+                            })
+                    else:
+                        pass
+        items.sort(key=lambda x: x['vote'],reverse=True)         
+        print(items)    
+    return render_template("read.html",items=items,param=param)
 
 
 app.run()
