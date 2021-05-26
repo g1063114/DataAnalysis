db637b64690f2ad53667a5962a32abe19613de07
jinho <g1063114@naver.com>
2021-05-26 21:55:56 +0900

    pandas test... read csv

--- pandas_dataframe.py
+++ pandas_dataframe.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-train_data = pd.read_csv('./train.csv')
+#train_data = pd.read_csv('./train.csv')
 
 #print(train_data.head())
 #print(train_data.shape)    
@@ -9,15 +9,24 @@ train_data = pd.read_csv('./train.csv')
 #print(train_data.info())
 
 
-data = {'a':100,'b':200,'c':300}
-print(pd.DataFrame(data, index=['x','y','z']))
+# data = {'a':100,'b':200,'c':300}
+# print(pd.DataFrame(data, index=['x','y','z']))
 
 
-data2 = {'a':[1,2,3],'b':[4,5,6],'c':[10,11,12]}
-print(pd.DataFrame(data2, index=['x','y','z']))
+# data2 = {'a':[1,2,3],'b':[4,5,6],'c':[10,11,12]}
+# print(pd.DataFrame(data2, index=['x','y','z']))
 
-a = pd.Series([100,200,300],['a','b','c'])
-b = pd.Series([101,201,301],['a','b','c'])
-c = pd.Series([105,205,305],['a','b','c'])
+# a = pd.Series([100,200,300],['a','b','c'])
+# b = pd.Series([101,201,301],['a','b','c'])
+# c = pd.Series([105,205,305],['a','b','c'])
 
-print(pd.DataFrame([a,b,c]))
+# print(pd.DataFrame([a,b,c]))
+
+# train_data = pd.read_csv('./train.csv', sep=',')
+# print(train_data)
+
+# train_data = pd.read_csv('./train.csv', index_col='PassengerId')
+# print(train_data)
+
+train_data = pd.read_csv('./train.csv', usecols=['Survived','Name'])
+print(train_data)
